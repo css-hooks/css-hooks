e105cb7f7f077cbbd31914b35b2a22f4041a76bf
Nick Saunders <nick@saunde.rs>
2023-08-12 05:13:33 -0700

    Solid package

---
+++ packages/solid/src/index.ts
@@ -0,0 +1,21 @@
+import type { JSX } from "solid-js";
+import { createHooksFn, types } from "@hooks.css/core";
+
+/** @internal */
+export function stringifyValue(_: string, value: unknown): string | null {
+  if (typeof value === "string") {
+    return value;
+  }
+
+  if (typeof value === "number") {
+    return `${value}`;
+  }
+
+  return null;
+}
+
+export default createHooksFn<JSX.CSSProperties>()(
+  "kebab",
+  stringifyValue,
+  types
+);
