df66f00fd8b31295e0f043520abae802a7848822
Nick Saunders <nick@saunde.rs>
2023-12-03 10:13:50 -0700

    docs(example-react): new recommended config format

--- examples/react/src/css-hooks.ts
+++ examples/react/src/css-hooks.ts
@@ -1,9 +1,6 @@
 import { createHooks, recommended } from "@css-hooks/react";
 
-const [css, hooks] = createHooks({
+export const [hooks, css] = createHooks({
   ...recommended,
   dark: "@media (prefers-color-scheme: dark)",
 });
-
-export default hooks;
-export { css };
