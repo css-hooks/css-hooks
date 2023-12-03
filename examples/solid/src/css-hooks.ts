3cec9677b85568c61352ad3358caf80e204d3374
Nick Saunders <nick@saunde.rs>
2023-12-03 11:33:17 -0700

    docs(example-solid): new recommended config format

--- examples/solid/src/css-hooks.ts
+++ examples/solid/src/css-hooks.ts
@@ -1,9 +1,6 @@
 import { createHooks, recommended } from "@css-hooks/solid";
 
-const [css, hooks] = createHooks({
+export const [hooks, css] = createHooks({
   ...recommended,
   dark: "@media (prefers-color-scheme: dark)",
 });
-
-export default hooks;
-export { css };
