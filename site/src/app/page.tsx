2f9653061ead972f685bfbd5e418482edfd1d14b
Nick Saunders <nick@saunde.rs>
2023-12-08 08:34:28 -0700

    docs(site): homepage enhancements, layout and verbiage

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -91,7 +91,7 @@ export default async function Home() {
               display: "flex",
               flexDirection: "column",
               alignItems: "flex-start",
-              gap: "2rem",
+              gap: "3rem",
             }}
           >
             <Logo size="2rem" />
@@ -101,7 +101,10 @@ export default async function Home() {
               <Typography variant="bold3XL">
                 {({ className, style, ...restProps }) =>
                   exhausted(restProps) && (
-                    <h1 className={className} style={style}>
+                    <h1
+                      className={className}
+                      style={{ ...style, lineHeight: 1.25 }}
+                    >
                       Inline styles doing what we thought they couldn&apos;t.
                     </h1>
                   )
@@ -119,8 +122,8 @@ export default async function Home() {
                       }}
                     >
                       Hooks bring advanced CSS capabilities to native inline
-                      styles, with practically zero runtime, no build steps, and
-                      a tiny CSS footprint.
+                      styles, with extremely minimal runtime, no build steps,
+                      and a tiny CSS footprint.
                     </p>
                   )
                 }
@@ -165,7 +168,7 @@ export default async function Home() {
                       })}
                     >
                       <CodeSandboxIcon aria-hidden="true" />
-                      Try now
+                      Demo
                     </a>
                   )
                 }
@@ -251,7 +254,7 @@ export default async function Home() {
             headline="Predictable performance"
             iconPath="M425.7 118.25A240 240 0 0 0 76.32 447l.18.2c.33.35.64.71 1 1.05c.74.84 1.58 1.79 2.57 2.78a41.17 41.17 0 0 0 60.36-.42a157.13 157.13 0 0 1 231.26 0a41.18 41.18 0 0 0 60.65.06l3.21-3.5l.18-.2a239.93 239.93 0 0 0-10-328.76ZM240 128a16 16 0 0 1 32 0v32a16 16 0 0 1-32 0ZM128 304H96a16 16 0 0 1 0-32h32a16 16 0 0 1 0 32Zm48.8-95.2a16 16 0 0 1-22.62 0l-22.63-22.62a16 16 0 0 1 22.63-22.63l22.62 22.63a16 16 0 0 1 0 22.62Zm149.3 23.1l-47.5 75.5a31 31 0 0 1-7 7a30.11 30.11 0 0 1-35-49l75.5-47.5a10.23 10.23 0 0 1 11.7 0a10.06 10.06 0 0 1 2.3 14Zm31.72-23.1a16 16 0 0 1-22.62-22.62l22.62-22.63a16 16 0 0 1 22.63 22.63Zm65.88 227.6ZM416 304h-32a16 16 0 0 1 0-32h32a16 16 0 0 1 0 32Z"
           >
-            Hooks are pure, don&apos;t depend on runtime CSS magic, and avoid
+            Hooks are pure, don&apos;t depend on style injection, and avoid
             shipping large volumes of irrelevant, render-blocking CSS.
           </Feature>
           <Feature
