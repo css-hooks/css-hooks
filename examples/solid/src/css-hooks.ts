7fd7b14f3ab85d2ec7482331e00ee73c9c955ac3
Nick Saunders <nick@saunde.rs>
2023-12-08 15:40:12 -0700

    docs(example-solid): new naming convention

--- examples/solid/src/css-hooks.ts
+++
@@ -1,6 +0,0 @@
-import { createHooks, recommended } from "@css-hooks/solid";
-
-export const [hooks, css] = createHooks({
-  ...recommended,
-  dark: "@media (prefers-color-scheme: dark)",
-});
--- examples/solid/src/index.tsx
+++ examples/solid/src/index.tsx
@@ -1,5 +1,5 @@
 import "@fontsource/inter/latin-700.css";
-import { hooks } from "./css-hooks";
+import { hooks } from "./css";
 
 import { render } from "solid-js/web";
 import App from "./App";
