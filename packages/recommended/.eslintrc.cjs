14d830373bc7b82f9b705a02eb144385049f8917
Nick Saunders <nick@saunde.rs>
2023-12-07 21:09:26 -0700

    feat(recommended): provide configurable presets

---
+++ packages/recommended/.eslintrc.cjs
@@ -0,0 +1,17 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+  },
+};
