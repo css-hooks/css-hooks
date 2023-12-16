e4e759e1edbec5bf93627f00adfbe4b346632654
Nick Saunders <nick@saunde.rs>
2023-12-15 18:03:36 -0700

    docs(site): comment color

--- site/src/app/globals.css.ts
+++ site/src/app/globals.css.ts
@@ -1,171 +1,155 @@
-import {
-  blue20,
-  blue70,
-  gray20,
-  gray30,
-  gray40,
-  gray70,
-  gray80,
-  gray90,
-  green20,
-  green70,
-  purple20,
-  purple70,
-  red20,
-  red70,
-  white,
-} from "varsace";
+import * as V from "varsace";
 
 const css = `
   /* syntax highlighting */
 
   .syntax-highlighter {
-    color: ${gray90};
+    color: ${V.gray90};
   }
 
   .syntax-highlighter .keyword {
-    color: ${purple70};
+    color: ${V.purple70};
   }
 
   .syntax-highlighter .punctuation {
-    color: ${gray80};
+    color: ${V.gray80};
   }
 
   .syntax-highlighter .attr-name {
-    color: ${blue70};
+    color: ${V.blue70};
   }
 
   .syntax-highlighter .attr-value {
-    color: ${red70};
+    color: ${V.red70};
   }
 
   .syntax-highlighter .script {
-    color: ${gray90};
+    color: ${V.gray90};
   }
 
   .syntax-highlighter .operator {
-    color: ${gray70};
+    color: ${V.gray70};
   }
 
   .syntax-highlighter .plain-text {
-    color: ${gray90};
+    color: ${V.gray90};
   }
 
   .syntax-highlighter .tag {
-    color: ${green70};
+    color: ${V.green70};
   }
 
   .syntax-highlighter .function {
-    color: ${purple70};
+    color: ${V.purple70};
   }
 
   .syntax-highlighter .language-javascript {
-    color: ${gray90};
+    color: ${V.gray90};
   }
 
   .syntax-highlighter .language-tsx {
-    color: ${gray90};
+    color: ${V.gray90};
   }
 
   /* Highlighting specific cases */
 
   .syntax-highlighter .language-tsx .token.string {
-    color: ${blue70};
+    color: ${V.blue70};
   }
 
-  .syntax-highlighter .language-tsx .token.comment {
-    color: ${gray70};
+  .syntax-highlighter .token.comment {
+    color: ${V.gray60};
   }
 
   .syntax-highlighter .language-tsx .token.plain-text {
-    color: ${gray90};
+    color: ${V.gray90};
   }
 
   .syntax-highlighter .language-tsx .token.script-punctuation {
-    color: ${gray80};
+    color: ${V.gray80};
   }
 
   .syntax-highlighter .language-tsx .token.function-variable {
-    color: ${blue70};
+    color: ${V.blue70};
   }
 
   .syntax-highlighter .language-tsx .token.boolean {
-    color: ${purple70};
+    color: ${V.purple70};
   }
 
   @media (prefers-color-scheme: dark) {
     .syntax-highlighter {
-      color: ${white};
+      color: ${V.white};
     }
 
     .syntax-highlighter .keyword {
-      color: ${purple20};
+      color: ${V.purple20};
     }
 
     .syntax-highlighter .punctuation {
-      color: ${gray30};
+      color: ${V.gray30};
     }
 
     .syntax-highlighter .attr-name {
-      color: ${blue20};
+      color: ${V.blue20};
     }
 
     .syntax-highlighter .attr-value {
-      color: ${red20};
+      color: ${V.red20};
     }
 
     .syntax-highlighter .script {
-      color: ${white};
+      color: ${V.white};
     }
 
     .syntax-highlighter .operator {
-      color: ${gray20};
+      color: ${V.gray20};
     }
 
     .syntax-highlighter .plain-text {
-      color: ${white};
+      color: ${V.white};
     }
 
     .syntax-highlighter .tag {
-      color: ${green20};
+      color: ${V.green20};
     }
 
     .syntax-highlighter .function {
-      color: ${purple20};
+      color: ${V.purple20};
     }
 
     .syntax-highlighter .language-javascript {
-      color: ${white};
+      color: ${V.white};
     }
 
     .syntax-highlighter .language-tsx {
-      color: ${white};
+      color: ${V.white};
     }
 
     /* Highlighting specific cases */
 
     .syntax-highlighter .language-tsx .token.string {
-      color: ${blue20};
+      color: ${V.blue20};
     }
 
-    .syntax-highlighter .language-tsx .token.comment {
-      color: ${gray40};
+    .syntax-highlighter .token.comment {
+      color: ${V.gray40};
     }
 
     .syntax-highlighter .language-tsx .token.plain-text {
-      color: ${white};
+      color: ${V.white};
     }
 
     .syntax-highlighter .language-tsx .token.script-punctuation {
-      color: ${gray30};
+      color: ${V.gray30};
     }
 
     .syntax-highlighter .language-tsx .token.function-variable {
-      color: ${blue20};
+      color: ${V.blue20};
     }
 
     .syntax-highlighter .language-tsx .token.boolean {
-      color: ${purple20};
+      color: ${V.purple20};
     }
   }
 
