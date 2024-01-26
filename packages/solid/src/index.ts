78b1642b007c6c45b00d6b879a9b2375d4b0a887
Nick Saunders <nick@saunde.rs>
2024-01-26 13:34:23 -0700

    feat(solid)!: drop deprecated recommended export

--- packages/solid/src/index.ts
+++ packages/solid/src/index.ts
@@ -1,13 +1,5 @@
 import type { JSX } from "solid-js";
-import {
-  buildHooksSystem,
-  recommended as coreRecommended,
-  genericStringify,
-} from "@css-hooks/core";
-import { A, U } from "ts-toolbelt";
-
-/** @internal */
-export const stringifyValue = genericStringify;
+import { buildHooksSystem } from "@css-hooks/core";
 
 /**
  * Creates the hooks specified in the configuration.
@@ -16,33 +8,4 @@ export const stringifyValue = genericStringify;
  *
  * @returns The `hooks` CSS required to enable the configured hooks, along with the corresponding `css` function for use in components.
  */
-export const createHooks = buildHooksSystem<JSX.CSSProperties>(stringifyValue);
-
-type CamelToKebab<
-  S extends string,
-  Acc extends string = "",
-> = S extends `${infer H}${infer T}`
-  ? CamelToKebab<T, `${Acc}${H extends Capitalize<H> ? `-${Lowercase<H>}` : H}`>
-  : Acc;
-
-function keybab<R extends Record<string, unknown>, K extends keyof R = keyof R>(
-  r: R,
-): A.Compute<
-  U.IntersectOf<K extends string ? Record<CamelToKebab<K>, R[K]> : never>
-> {
-  /* eslint-disable @typescript-eslint/no-unsafe-return,@typescript-eslint/no-explicit-any */
-  return Object.fromEntries(
-    Object.entries(r).map(([key, value]) => [
-      key.replace(/[A-Z]/g, x => `-${x.toLowerCase()}`),
-      value,
-    ]),
-  ) as any;
-  /* eslint-enable @typescript-eslint/no-unsafe-return,@typescript-eslint/no-explicit-any */
-}
-
-/**
- * A list of hooks offered as a "sensible default" to solve the most common use cases.
- *
- * @deprecated Use the `@css-hooks/recommended` package instead.
- */
-export const recommended = keybab(coreRecommended);
+export const createHooks = buildHooksSystem<JSX.CSSProperties>();
