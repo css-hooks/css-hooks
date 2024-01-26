4c7b3b65dba5efbb71ad7d07321766892066ca8f
Nick Saunders <nick@saunde.rs>
2024-01-26 13:34:23 -0700

    feat(solid)!: drop deprecated recommended export

--- packages/solid/src/index.test.ts
+++ packages/solid/src/index.test.ts
@@ -1,17 +1,1 @@
-import assert from "node:assert";
-import { describe, it } from "node:test";
-import { stringifyValue } from "./index.js";
-
-describe("`stringifyValue` function", () => {
-  it("returns a string as-is", () => {
-    ["a", "red", ""].forEach(x => {
-      assert.equal(stringifyValue("", x), x);
-    });
-  });
-
-  it("returns numbers as direct string equivalents", () => {
-    ["line-height", "order", "z-index"].forEach(propertyName => {
-      assert.equal(stringifyValue(propertyName, 1.5), "1.5");
-    });
-  });
-});
+console.log("No tests currently needed");
