9ce5526903d89b3de63aa27a866fbcbe979ee037
Nick Saunders <nick@saunde.rs>
2024-01-30 11:47:11 -0700

    refactor(preact): js impl

---
+++ packages/preact/src/index.d.ts
@@ -0,0 +1,28 @@
+/**
+ *  CSS Hooks for Preact
+ *
+ * @packageDocumentation
+ */
+
+import type { JSX } from "preact";
+import type { CreateHooksFn } from "@css-hooks/core";
+
+/**
+ * A version of Preact's {@link JSX.CSSProperties} type that admits a `match`
+ * function.
+ */
+export type CSSProperties = JSX.DOMCSSProperties & { cssText?: string | null };
+
+/**
+ * A {@link @css-hooks/core#CreateHooksFn} configured to use Preact's
+ * `JSX.CSSProperties` type and logic for converting CSS values into strings.
+ *
+ * @public
+ */
+export const createHooks: CreateHooksFn<CSSProperties>;
+
+/** @internal */
+declare function _stringifyValue(
+  propertyName: string,
+  value: unknown,
+): string | null;
