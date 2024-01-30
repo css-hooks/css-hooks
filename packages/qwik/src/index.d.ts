0194fb0f85382f1118a58ca706d7ca1b096819e1
Nick Saunders <nick@saunde.rs>
2024-01-30 12:06:38 -0700

    refactor(qwik): js impl

---
+++ packages/qwik/src/index.d.ts
@@ -0,0 +1,25 @@
+/**
+ *  CSS Hooks for Qwik
+ *
+ * @packageDocumentation
+ */
+
+import type { CSSProperties } from "@builder.io/qwik";
+import type { CreateHooksFn } from "@css-hooks/core";
+
+/**
+ * A {@link @css-hooks/core#CreateHooksFn} configured to use Qwik's
+ * `CSSProperties` type and logic for converting CSS values into strings.
+ *
+ * @public
+ */
+export const createHooks: CreateHooksFn<CSSProperties>;
+
+/** @internal */
+declare function _stringifyValue(
+  propertyName: string,
+  value: unknown,
+): string | null;
+
+/** @internal */
+export const _unitlessNumbers: Set<string>;
