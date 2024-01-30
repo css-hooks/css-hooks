9380208143b4da90397d52deec90c85d96fb2ebc
Nick Saunders <nick@saunde.rs>
2024-01-30 16:46:47 -0700

    refactor(example-react): v2 update

--- examples/react/src/main.tsx
+++ examples/react/src/main.tsx
@@ -1,5 +1,5 @@
 import "@fontsource/inter/latin-700.css";
-import { hooks } from "./css";
+import { styleSheet } from "./css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -7,7 +7,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <style dangerouslySetInnerHTML={{ __html: hooks }} />
+    <style dangerouslySetInnerHTML={{ __html: styleSheet() }} />
     <App />
   </React.StrictMode>,
 );
