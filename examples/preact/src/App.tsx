3294777cd2f3e930bffe3bf333af33bc7cac5b71
Nick Saunders <nick@saunde.rs>
2024-01-30 16:09:17 -0700

    refactor(example-preact): v2 update

--- examples/preact/src/App.tsx
+++ examples/preact/src/App.tsx
@@ -13,14 +13,16 @@ function App() {
         left: 0,
         background: V.white,
         color: V.black,
-        "@media (prefers-color-scheme: dark)": {
-          background: V.black,
-          color: V.white,
-        },
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
+        match: on => [
+          on("@media (prefers-color-scheme: dark)", {
+            background: V.black,
+            color: V.white,
+          }),
+        ],
       })}
     >
       <div style={{ fontSize: 192 }}>
@@ -39,21 +41,23 @@ function App() {
           color: V.white,
           padding: "0.5em 0.75em",
           display: "inline-block",
-          "&:hover": {
-            background: V.blue40,
-          },
-          "&:active": {
-            background: V.red40,
-          },
-          "@media (prefers-color-scheme: dark)": {
-            background: V.gray70,
-            "&:hover": {
+          match: (on, { all }) => [
+            on("&:hover", {
+              background: V.blue40,
+            }),
+            on("&:active", {
+              background: V.red40,
+            }),
+            on("@media (prefers-color-scheme: dark)", {
+              background: V.gray70,
+            }),
+            on(all("@media (prefers-color-scheme: dark)", "&:hover"), {
               background: V.blue50,
-            },
-            "&:active": {
+            }),
+            on(all("@media (prefers-color-scheme: dark)", "&:active"), {
               background: V.red50,
-            },
-          },
+            }),
+          ],
         })}
       >
         Get started
