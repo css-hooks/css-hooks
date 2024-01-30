35f958237297a2769ec9ddb8aeb79dad459dd44e
Nick Saunders <nick@saunde.rs>
2024-01-30 16:09:17 -0700

    refactor(example-preact): v2 update

--- examples/preact/src/main.tsx
+++ examples/preact/src/main.tsx
@@ -2,12 +2,12 @@ import "@fontsource/inter/latin-700.css";
 
 import { render } from "preact";
 import App from "./App";
-import { hooks } from "./css";
+import { styleSheet } from "./css";
 
 render(
   <>
     {/* eslint-disable-next-line react/no-danger */}
-    <style dangerouslySetInnerHTML={{ __html: hooks }} />
+    <style dangerouslySetInnerHTML={{ __html: styleSheet() }} />
     <App />
   </>,
   document.getElementById("root")!,
