ecda8873ccb6301cc9cf2a947f7131a350559b50
Nick Saunders <nick@saunde.rs>
2024-02-19 11:10:45 -0700

    refactor(site): suggest edit link

--- site/src/components/icons.tsx
+++ site/src/components/icons.tsx
@@ -24,6 +24,15 @@ export const DarkMode = component$(() => (
   </svg>
 ));
 
+export const Edit = component$(() => (
+  <svg viewBox="0 -960 960 960" width="1em" height="1em">
+    <path
+      d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"
+      fill="currentColor"
+    />
+  </svg>
+));
+
 export const ExpandMore = component$(() => (
   <svg height="1em" width="1em" viewBox="0 -960 960 960">
     <path
--- site/src/routes/docs/[...path]/index.tsx
+++ site/src/routes/docs/[...path]/index.tsx
@@ -7,7 +7,11 @@ import {
   z,
 } from "@builder.io/qwik-city";
 import matter from "gray-matter";
+import { Anchor } from "~/components/anchor";
 import * as Markdown from "~/markdown";
+import * as V from "varsace";
+import * as Icon from "~/components/icons";
+import { css } from "~/css";
 
 const documents = import.meta.glob("../../../../../docs/**/*.md", {
   import: "default",
@@ -35,6 +39,7 @@ export const useDocument = routeLoader$(async requestEvent => {
     const isIndex = indexPath in documents;
     const markdown = matter(await documents[isIndex ? indexPath : altPath]());
     return {
+      path: (isIndex ? indexPath : altPath).replace(/^(.*)?\/docs\//, ""),
       failed: false,
       isIndex,
       content: (await Markdown.render(markdown.content)) || "",
@@ -68,7 +73,7 @@ export default component$(() => {
   const location = useLocation();
   const document = useDocument();
   return (
-    <>
+    <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
       {document.value.isIndex ? undefined : (
         <base href={location.url.toString().replace(/\/$/, "")} />
       )}
@@ -77,7 +82,43 @@ export default component$(() => {
           document.value.content || document.value.errorMessage
         }
       />
-    </>
+      {document.value.failed ? undefined : (
+        <>
+          <hr
+            style={css({
+              border: 0,
+              width: "100%",
+              height: 1,
+              background: V.gray10,
+              on: $ => [
+                $("@media (prefers-color-scheme: dark)", {
+                  background: V.gray80,
+                }),
+              ],
+            })}
+          />
+          <Anchor
+            href={`https://github.com/css-hooks/css-hooks/edit/master/docs/${document.value.path}`}
+          >
+            <div
+              style={{
+                display: "inline-flex",
+                alignItems: "center",
+                gap: "0.25em",
+              }}
+            >
+              <span
+                style={{ fontSize: "1.125em", display: "inline-flex" }}
+                aria-hidden="true"
+              >
+                <Icon.Edit />
+              </span>
+              <span>Suggest an edit</span>
+            </div>
+          </Anchor>
+        </>
+      )}
+    </div>
   );
 });
 
