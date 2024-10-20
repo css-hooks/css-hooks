927d38c57ac74abe2b61857e52813ee4568f6fcb
Nick Saunders <nick@saunde.rs>
2024-10-20 12:02:44 -0700

    fix(solid): re-export core types

--- packages/solid/src/index.ts
+++ packages/solid/src/index.ts
@@ -7,6 +7,8 @@
 import { buildHooksSystem } from "@css-hooks/core";
 import type { JSX } from "solid-js";
 
+export type * from "@css-hooks/core";
+
 /**
  * A {@link @css-hooks/core#CreateHooksFn} configured to use Solid's
  * `JSX.CSSProperties` type and logic for converting CSS values into strings.
