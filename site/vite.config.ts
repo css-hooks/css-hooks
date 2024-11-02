6bd6570ef83f0da04691c25481ea7e4ee4508277
Nick Saunders <nick@saunde.rs>
2024-11-02 10:57:53 -0700

    refactor(site): fix type error in vite config

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,7 +1,8 @@
 import react from "@vitejs/plugin-react";
+import type { PluginOption } from "vite";
 import { defineConfig } from "vite";
 import { stringPlugin } from "vite-string-plugin";
 
 export default defineConfig({
-  plugins: [react(), stringPlugin()],
+  plugins: [react(), stringPlugin() as unknown as PluginOption],
 });
