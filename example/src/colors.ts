806284d0033c17680a7313698fcd3bf05f8a411b
Nick Saunders <nick@saunde.rs>
2025-01-13 06:36:45 -0700

    refactor(example): remove unused colors

--- example/src/colors.ts
+++ example/src/colors.ts
@@ -21,13 +21,7 @@ function color(chroma: number, hue: number) {
 }
 
 export const gray = color(0.02, 251);
-export const pink = color(0.18, 350);
-export const purple = color(0.17, 294);
 export const blue = color(0.18, 258);
-export const teal = color(0.08, 202);
-export const green = color(0.14, 147);
-export const yellow = color(0.16, 92);
-export const orange = color(0.18, 46);
 export const red = color(0.19, 20);
 export const black = "oklch(9.5% 0.01 260)";
 export const white = "oklch(100% 0.01 260)";
