e4752de519d8f089fee326d7196951b6918103e5
Nick Saunders <nick@saunde.rs>
2026-09-04 16:26:52 -0700

    feat: refine site typography and spacing

--- site/src/routes/doc.tsx
+++ site/src/routes/doc.tsx
@@ -67,7 +67,7 @@ function MenuList({ children }: { children: ReactNode }) {
           paddingLeft: 0,
         },
         on(".group &.group", {
-          paddingLeft: "2em",
+          paddingLeft: 32,
         }),
       )}
     >
@@ -83,7 +83,7 @@ function MenuItem({
   level = 0,
 }: MenuItem & { level?: number }) {
   return (
-    <li style={{ marginTop: level === 0 ? "1em" : "0.4em" }}>
+    <li style={{ marginTop: level === 0 ? 16 : 8 }}>
       <NavLink
         to={pathname}
         end
@@ -145,7 +145,7 @@ function createHeading(level: 1 | 2 | 3 | 4 | 5 | 6, style: CSSProperties) {
                 }),
               )}
             >
-              <span style={{ fontSize: "0.666em", lineHeight: "1.5em" }}>
+              <span style={{ fontSize: "0.666em", lineHeight: 1.5 }}>
                 {step}
               </span>
             </span>
@@ -178,11 +178,14 @@ function createHeading(level: 1 | 2 | 3 | 4 | 5 | 6, style: CSSProperties) {
     return (
       <Tag
         className="group"
-        style={{
-          lineHeight: 1.25,
-          ...style,
-          ...styleProp,
-        }}
+        style={pipe(
+          {
+            lineHeight: 1.25,
+            ...style,
+            ...styleProp,
+          },
+          on("&:first-child", { marginBlockStart: 0 }),
+        )}
         {...restProps}
       >
         <span
@@ -276,7 +279,7 @@ export async function loader({ params }: Route.LoaderArgs) {
                   borderWidth: 1,
                   borderColor: "inherit",
                   borderStyle: "solid",
-                  padding: "calc(0.375em - 0.5px) 0.75em",
+                  padding: "calc(6px - 0.5px) 12px",
                 },
                 on(not(or(dark, ".group:nth-child(even) &")), {
                   background: white,
@@ -393,7 +396,7 @@ export async function loader({ params }: Route.LoaderArgs) {
                     {
                       display: "inline-flex",
                       alignItems: "center",
-                      gap: "0.375em",
+                      gap: 8,
                     },
                     on(warning, {
                       color: warningColor(50),
@@ -441,10 +444,11 @@ export async function loader({ params }: Route.LoaderArgs) {
                   borderWidth: 0,
                   borderLeftWidth: "8px",
                   borderStyle: "solid",
-                  padding: "0.1px 1em",
+                  paddingBlock: "0.1px",
+                  paddingInline: 16,
                   marginLeft: 0,
                   marginRight: 0,
-                  marginBlock: "1.5rem",
+                  marginBlock: 24,
                   borderColor: gray(50),
                   color: gray(70),
                   background: white,
@@ -503,40 +507,50 @@ export async function loader({ params }: Route.LoaderArgs) {
           );
         },
         h1: createHeading(1, {
-          fontSize: "3rem",
+          fontSize: "3em",
           fontWeight: 400,
-          marginBlock: "1.25rem",
+          marginBlockStart: 48,
+          marginBlockEnd: 16,
         }),
         h2: createHeading(2, {
-          fontSize: "2.2rem",
+          fontSize: "2em",
           fontWeight: 400,
-          marginBlock: "1.75rem",
+          marginBlockStart: 40,
+          marginBlockEnd: 12,
         }),
         h3: createHeading(3, {
-          fontSize: "1.8rem",
+          fontSize: "1.5em",
           fontWeight: 400,
-          marginBlock: "2rem",
+          lineHeight: 4 / 3,
+          marginBlockStart: 32,
+          marginBlockEnd: 8,
         }),
         h4: createHeading(4, {
-          fontSize: "1.4rem",
+          fontSize: "1.25em",
           fontWeight: 400,
-          marginBlock: "2.25rem",
+          lineHeight: 1.4,
+          marginBlockStart: 28,
+          marginBlockEnd: 8,
         }),
         h5: createHeading(5, {
-          fontSize: "1rem",
+          fontSize: "1em",
           fontWeight: 700,
-          marginBlock: "2.5rem",
+          marginBlockStart: 24,
+          marginBlockEnd: 8,
         }),
         h6: createHeading(6, {
-          fontSize: "0.75rem",
+          fontSize: "0.75em",
           fontWeight: 700,
-          marginBlock: "2.625rem",
+          lineHeight: 4 / 3,
+          marginBlockStart: 24,
+          marginBlockEnd: 8,
         }),
         hr: ({ style, ...restProps }) => (
           <hr
             style={pipe(
               {
-                margin: "2rem 0",
+                marginBlock: 32,
+                marginInline: 0,
                 border: 0,
                 width: "100%",
                 height: 1,
@@ -554,11 +568,13 @@ export async function loader({ params }: Route.LoaderArgs) {
           <p
             style={pipe(
               {
-                margin: "1em 0",
+                marginBlock: 16,
+                marginInline: 0,
                 ...style,
               },
               on(or("th > &:only-child", "td > &:only-child"), {
-                margin: 0,
+                marginBlock: 0,
+                marginInline: 0,
               }),
             )}
             {...restProps}
@@ -568,9 +584,10 @@ export async function loader({ params }: Route.LoaderArgs) {
           <pre
             style={pipe(
               {
-                padding: "1rem 1.5rem",
+                paddingBlock: 16,
+                paddingInline: 24,
                 background: white,
-                marginBlock: "1.5rem",
+                marginBlock: 24,
                 overflow: "auto",
                 ...style,
               },
@@ -658,9 +675,11 @@ export default function Doc({ loaderData: doc }: Route.ComponentProps) {
             {
               display: "flex",
               alignItems: "center",
-              padding: "1rem 1.25rem",
-              gap: "0.25rem",
-              fontSize: "1.25rem",
+              paddingBlock: 16,
+              paddingInline: 20,
+              gap: 4,
+              fontSize: "1.25em",
+              lineHeight: 1.2,
               background: gray(12),
               color: gray(55),
               outlineWidth: 0,
@@ -724,22 +743,22 @@ export default function Doc({ loaderData: doc }: Route.ComponentProps) {
         <div
           style={pipe(
             {
-              marginTop: "0.5em",
+              marginTop: 8,
               paddingTop: 0,
-              paddingRight: "1.75rem",
-              paddingBottom: "1.75rem",
-              paddingLeft: "1.75rem",
+              paddingRight: 28,
+              paddingBottom: 28,
+              paddingLeft: 28,
             },
             on(not(or(":has(:checked) + &", "@media (width >= 44em)")), {
               display: "none",
             }),
             on("@media (width >= 44em)", {
               position: "fixed",
-              marginTop: "-0.5em",
-              paddingTop: "2rem",
-              paddingRight: "2rem",
-              paddingBottom: "2rem",
-              paddingLeft: "2rem",
+              marginTop: -8,
+              paddingTop: 32,
+              paddingRight: 32,
+              paddingBottom: 32,
+              paddingLeft: 32,
             }),
           )}
         >
@@ -768,13 +787,14 @@ export default function Doc({ loaderData: doc }: Route.ComponentProps) {
         <div
           style={pipe(
             {
-              width: "calc(100% - 4rem)",
+              width: "calc(100% - 64px)",
               maxWidth: "88ch",
               margin: "auto",
-              padding: "1rem 0",
+              paddingBlock: 16,
+              paddingInline: 0,
             },
             on("@media (width >= 69em)", {
-              width: "calc(100% - 8rem)",
+              width: "calc(100% - 128px)",
             }),
           )}
         >
@@ -782,7 +802,7 @@ export default function Doc({ loaderData: doc }: Route.ComponentProps) {
             style={{
               display: "flex",
               flexDirection: "column",
-              gap: "2rem",
+              gap: 32,
             }}
           >
             <div
@@ -795,7 +815,7 @@ export default function Doc({ loaderData: doc }: Route.ComponentProps) {
                   display: "flex",
                   flexDirection: "column",
                   alignItems: "flex-start",
-                  gap: "1rem",
+                  gap: 16,
                 }}
               >
                 <hr
@@ -832,7 +852,7 @@ export default function Doc({ loaderData: doc }: Route.ComponentProps) {
             )}
           </div>
         </div>
-        <div style={{ height: "2rem" }} />
+        <div style={{ height: 32 }} />
       </main>
     </div>
   );
